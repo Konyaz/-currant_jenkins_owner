a1049013021a930ccfab68d193588672c4ec34de
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-04-14 17:39:28 +0900

    add

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import config.DriverConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,6 +13,9 @@ import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentHelper.*;
 
 public class TestBase {
+
+    static DriverConfig driverConfig = ConfigFactory.create(DriverConfig.class);
+
     @BeforeAll
     static void setup() {
         addListener("AllureSelenide", new AllureSelenide());
@@ -18,7 +23,27 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        //        gradle clean test -Dweb.browser=opera
+        Configuration.browser = System.getProperty("web.browser", "chrome");
+
+        // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        String remoteWebDriver = System.getProperty("remote.web.driver");
+
+        if (remoteWebDriver != null) {
+            String user = driverConfig.remoteWebUser();
+            String password = driverConfig.remoteWebPassword();
+            Configuration.remote = String.format(remoteWebDriver, user, password);
+
+            System.out.println(user);
+            System.out.println(password);
+            System.out.println(remoteWebDriver);
+            System.out.println(String.format(remoteWebDriver, user, password));
+
+            //        gradle clean test
+//        gradle clean test -Dremote.web.driver="https://user1:1234@selenoid.autotests.cloud/wd/hub/"
+//        gradle clean test -Dremote.web.driver="https://%s:%s@selenoid.autotests.cloud/wd/hub/"
+        }
     }
 
     @AfterEach
